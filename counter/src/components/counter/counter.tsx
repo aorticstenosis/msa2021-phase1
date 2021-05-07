f9c969c4bcb949b25f5411ae9866bf3103ccc783
aorticstenosis <hcui006@gmail.com>
2021-05-07 13:25:09 +1200

    added images

--- counter/src/components/counter/counter.tsx
+++ counter/src/components/counter/counter.tsx
@@ -5,13 +5,27 @@ import CounterProps from "./counterProps";
 const Counter: React.FC<CounterProps> = ({counterTitle}) => {
    
     const [count, setCount] = React.useState(5);
-   
     const increment = () => {
-        setCount(count+1)
+        setCount(count + 1)
     }
+    const decrement = () => {
+        setCount(count - 1)
+    }
+    
+    const [imageUrl, setImageUrl] = React.useState<string | null>(null);
+    React.useEffect(() => {
+        if (count >= 10) {
+            setImageUrl("/super-secret-image.jpg")
+        } else if (count <= 0) {
+            setImageUrl("/DO-NOT-OPEN.png")
+        } else {
+            setImageUrl(null)
+        }
+    }, [count]);
+
     return <div className="counter-wrapper">
     <div className="counter">
-        <button className="counter-btn">
+            <button className="counter-btn" onClick={decrement}>
             {"<"}
         </button>
         <div className="counter-label">
@@ -19,7 +33,10 @@ const Counter: React.FC<CounterProps> = ({counterTitle}) => {
         </div>
         <button className="counter-btn" onClick={increment}>
             {">"}
-        </button>
+            </button>
+        <div className="counter-image">
+            {imageUrl != null ? <img alt="some string" src={imageUrl} /> : null}
+        </div>
     </div>
 </div>
 }
