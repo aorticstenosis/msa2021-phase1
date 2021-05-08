817f72a22c43fcd6909fb551c7b714ccb0cee3be
aorticstenosis <hcui006@gmail.com>
2021-05-08 19:05:22 +1200

    deleted redundant code

--- counter/src/App.tsx
+++ counter/src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.css';
-import PadlockTable from './components/counter/padlockTable';
+import PadlockTable from './components/padlock/padlockTable';
 
 
 function App() {
-  const password = "000"
-  
   return <div className="App">
     <h3>Choose the password to unlock!</h3>
     <PadlockTable />
